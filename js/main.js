e7e3301cf181b66508ccd897c3131ea577743468
tamybl <tamara.bravo.l@gmail.com>
2018-09-04 11:45:11 -0300

    agregando informacion de productos y contenedor de precios, correcciones en responsive

--- js/main.js
+++ js/main.js
@@ -1,32 +1,34 @@
+var delta;
+
 const handleMove = () => {
-    // Variables Globales
+    // contenedor principal
     const articleWrap = document.getElementById('csl');
-    let delta = articleWrap.clientWidth;
-    let inicialsize = articleWrap.clientWidth;
+    delta = articleWrap.clientWidth;
     let element = document.getElementById("draggable-handle");
     let image = document.querySelector('#csl .csl_layer.slide-top');
-    
 
     window.addEventListener('resize', function () {
         delta = articleWrap.clientWidth;
+        console.log(delta);
         // Evita que al adaptar el tamaño de pantalla, el manejador se salga del contenedor.
+
         if (delta - 15 <= element.offsetLeft) {
-            image.style.width = delta - 15 + "px";
-            element.style.left = delta - 15 + "px";
+            image.style.width = delta - 4 + "px";
+            element.style.left = delta - 4 + "px";
         }
-        if (element.offsetLeft <= 15 ) {
-            image.style.width = 15 + "px";
-            element.style.left =  15 + "px";
+        if (element.offsetLeft <= 4 ) {
+            image.style.width = 4 + "px";
+            element.style.left =  4 + "px";
         }
     })
-    dragElement(element, delta, image);
-}
 
+    dragElement(document.getElementById("draggable-handle"));
+}
 document.addEventListener('DOMContentLoaded', handleMove);
 
-
-function dragElement(elmnt, delta, image) {
+function dragElement(elmnt) {
     let [initialPos, finalPos] = [0, 0];
+    let image = document.querySelector('#csl .csl_layer.slide-top');
     elmnt.onmousedown = dragMouseDown;
     elmnt.addEventListener('touchstart', dragMouseDown, { passive: false });
 
@@ -38,46 +40,34 @@ function dragElement(elmnt, delta, image) {
         // Llama a la funcion cuando el mouse se mueve
         document.onmousemove = elementDrag;
         document.addEventListener('touchmove', elementDrag, { passive: false });
-
-
     }
 
     function elementDrag(e) {
         e = e || window.event;
         e.preventDefault();
-        /* Posicion condicional, si la primera es indefinida toma la segunda opcion. La primera corresponde a la posicion en vista en desktop y la segunda dispositivos touch (moviles y tablets)
-        */
         let currentPos =  e.clientX || e.touches[0].clientX;
-        // calcular la nueva posicion del puntero y de la imagen
         initialPos = finalPos - currentPos;
         finalPos = currentPos;
         image.style.width = (elmnt.offsetLeft - initialPos) + "px";
         elmnt.style.left = (elmnt.offsetLeft - initialPos) + "px";
-        // console.log(finalPos);
         console.log(elmnt.offsetLeft);
         // Estableciendo limites del elemento arrastrable
-        if (delta - 15 <= elmnt.offsetLeft) {
-            image.style.width = delta - 15 + "px";
-            elmnt.style.left = delta - 15 + "px";
-            document.onmousemove = null;
+        if (delta - 2 <= elmnt.offsetLeft) {
+            image.style.width = delta - 2 + "px";
+            elmnt.style.left = delta - 2 + "px";
         }
-        if (elmnt.offsetLeft <= 15 ) {
-            image.style.width = 15 + "px";
-            elmnt.style.left =  15 + "px";
-            document.onmousemove = null;
+        if (elmnt.offsetLeft <= 2 ) {
+            image.style.width = 2 + "px";
+            elmnt.style.left =  2 + "px";
         }
     }
 
     function closeDragElement() {
-        /* Detener el evento cuando el mouse es soltado */
         document.onmouseup = null;
         document.onmousemove = null;
-        // document.querySelector('#csl .csl_layer.slide-top').style.width = (image.clientWidth/delta)*100 + "%";
-        // document.getElementById("draggable-handle").style.left = (elmnt.offsetLeft/delta)*100 + "%";
-        // document.getElementById("draggable-handle").style.transform = "translate(-" + (elmnt.offsetLeft)*100 + ", -50%);";
-            // elmnt.style.left = delta - 15 + "px";
+    }
 
+    function check(delta, img) {
+            
     }
 }
-
-
